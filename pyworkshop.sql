2ef83d887df420dd6b361a9509348e5d76ab4240
Rahul Jain <rjain.rahul5@live.com>
2017-10-09 19:23:24 +0530

    SQL

---
+++ pyworkshop.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 09, 2017 at 03:52 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toggle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pyworkshop`
+--
+
+CREATE TABLE `pyworkshop` (
+  `name` text CHARACTER SET utf8 NOT NULL,
+  `usn` varchar(12) CHARACTER SET utf8 NOT NULL,
+  `year` int(11) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
